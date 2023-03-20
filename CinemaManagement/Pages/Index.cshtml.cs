8ffeae838863c4255774ae7f3380ef4b90efc0be
lehuupphuc <phuclhhe151336@fpt.edu.vn>
2023-03-20 20:04:44 +0700

    Index
    Trang chủ

--- CinemaManagement/Pages/Index.cshtml.cs
+++ CinemaManagement/Pages/Index.cshtml.cs
@@ -1,20 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CinemaManagement.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CinemaManagement.Pages
 {
+    [BindProperties]
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly CenimaDBContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(CenimaDBContext context)
         {
-            _logger = logger;
+            _context=context;
+        }
+        public String Search { get; set; }
+        public List<Movie> movies { get; set; } 
+        public List<Genre> genres { get; set; }
+        public double? AvgRate(List<Rate> rt)
+        {
+            double? avgRate = 0;
+            foreach (Rate r in rt)
+            {
+                avgRate+=Math.Round((double)(r.NumericRating / rt.Count),2);
+            }
+            return avgRate;
         }
 
-        public void OnGet()
+        public List<Rate> GetRateByMovieId(int id)
         {
+            return _context.Rates.Where(r => r.MovieId == id).ToList();
+        }
+        public void OnGet(int id)
+        {
+            if(id == 0)
+            {
+                movies = _context.Movies.Include("Genre").ToList();
+                genres = _context.Genres.ToList();                
+            }
+            else
+            {
+                movies = _context.Movies.Include("Genre").Where(x => x.GenreId == id).ToList();
+                genres = _context.Genres.ToList();
+            }
+        }
 
+        public void OnPost()
+        {
+            Search = Request.Form["Search"];
+            movies = _context.Movies.Include("Genre").Where(x => x.Title.Contains(Search)).ToList();
+            genres = _context.Genres.ToList();
         }
     }
 }
