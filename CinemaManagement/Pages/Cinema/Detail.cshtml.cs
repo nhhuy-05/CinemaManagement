d70b6466ccb75987e1a58db354f36cacddf88341
AT160201 <thutuoitd@gmail.com>
2023-03-22 01:28:19 +0700

    Add movie detail

--- CinemaManagement/Pages/Cinema/Detail.cshtml.cs
+++ CinemaManagement/Pages/Cinema/Detail.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CinemaManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CinemaManagement.Pages.Cinema
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private readonly CenimaDBContext _db;
+        public DetailModel(CenimaDBContext db)
         {
+            _db = db;
+        }
+        [BindProperty(SupportsGet = true)]
+        public Movie movie { get; set; }
+        public List<Rate> rates { get; set; }
+        public void OnGet(int id)
+        {
+            movie = _db.Movies.Include("Genre").Where(m => m.MovieId == id).SingleOrDefault();
+            rates = _db.Rates.Include("Person").Where(r => r.MovieId == id).ToList();
+        }
+
+
+        public double? AvgRate(List<Rate> rt)
+        {
+            double? avgRate = 0;
+            foreach (Rate r in rt)
+            {
+                avgRate += Math.Round((double)(r.NumericRating / rt.Count), 2);
+            }
+            return avgRate;
+        }
+        public List<Rate> GetRateByMovieId(int id)
+        {
+            return _db.Rates.Where(r => r.MovieId == id).ToList();
         }
     }
 }
