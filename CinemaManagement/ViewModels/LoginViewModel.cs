8bf5ca319e74c31d3d97018266c741488169eb78
hunghd0606 <110470333+hunghd0606@users.noreply.github.com>
2023-03-22 00:08:05 +0700

    ViewModel

---
+++ CinemaManagement/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaManagement.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập email!")]
+        [EmailAddress(ErrorMessage = "Email không đúng định dạng!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu!")]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "Mật khẩu phải có độ dài ít từ 1 đến 20 kí tự!")]
+        public string Password { get; set; }
+    }
+}
